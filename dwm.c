8b51c2a522ea53ca01698690f3155b5b5ba2bd07
Miles Alan <m@milesalan.com>
2019-08-19 18:49:08 -0500

    Bottom right corner for warp

--- dwm.c
+++ dwm.c
@@ -2674,7 +2674,7 @@ warp(const Client *c)
 	    (c->mon->topbar && !y))
 		return;
 
-	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
+	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w - 3, c->h - 16);
 }
 
 Client *
