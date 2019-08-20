b928452118b79d86b50f646a03668f6b0da32314
Miles Alan <m@milesalan.com>
2019-08-19 20:27:27 -0500

    Shift / Tab for -1 / +1 shiftview

--- config.h
+++ config.h
@@ -110,7 +110,10 @@ static Key keys[] = {
 
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.01} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.01} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+
+	{ MODKEY,                       XK_Tab,    shiftview,      {.i = 1} },
+	{ MODKEY|ShiftMask,             XK_Tab,    shiftview,      {.i = -1} },
+
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
