a18dd56803b67a3edb2a4e0089996d4473cba23f
Miles Alan <m@milesalan.com>
2019-08-24 22:17:39 -0500

    Pushup/pushdown works with master

--- dwm.c
+++ dwm.c
@@ -1691,32 +1691,69 @@ prevtiled(Client *c) {
 	return r;
 }
 
-void
-pushdown(const Arg *arg) {
-	Client *sel = selmon->sel, *c;
+Client *
+nextc(Client *c, float f) {
+	if(!f)
+		return nexttiled(c);
 
-	if(!sel || sel->isfloating || sel == nexttiled(selmon->clients))
+	for(; c && !ISVISIBLE(c); c = c->next);
+	return c;
+}
+
+static Client *
+prevc(Client *c, float f) {
+	Client *p, *r;
+
+	for(p = selmon->clients, r = NULL; c && p && p != c; p = p->next)
+		if((f || !p->isfloating) && ISVISIBLE(p))
+			r = p;
+	return r;
+}
+
+static void
+pushup(const Arg *arg) {
+	Client *sel = selmon->sel;
+	Client *c;
+
+	if(!sel || (sel->isfloating && !arg->f))
 		return;
-	if((c = nexttiled(sel->next))) {
+	if((c = prevc(sel, arg->f))) {
+		/* attach before c */
 		detach(sel);
-		sel->next = c->next;
+		sel->next = c;
+		if(selmon->clients == c)
+			selmon->clients = sel;
+		else {
+			for(c = selmon->clients; c->next != sel->next; c = c->next);
+			c->next = sel;
+		}
+	} else {
+		/* move to the end */
+		for(c = sel; c->next; c = c->next);
+		detach(sel);
+		sel->next = NULL;
 		c->next = sel;
 	}
 	focus(sel);
 	arrange(selmon);
 }
 
-void
-pushup(const Arg *arg) {
-	Client *sel = selmon->sel, *c;
+static void
+pushdown(const Arg *arg) {
+	Client *sel = selmon->sel;
+	Client *c;
 
-	if(!sel || sel->isfloating)
+	if(!sel || (sel->isfloating && !arg->f))
 		return;
-	if((c = prevtiled(sel)) && c != nexttiled(selmon->clients)) {
+	if((c = nextc(sel->next, arg->f))) {
+		/* attach after c */
 		detach(sel);
-		sel->next = c;
-		for(c = selmon->clients; c->next != sel->next; c = c->next);
-			c->next = sel;
+		sel->next = c->next;
+		c->next = sel;
+	} else {
+		/* move to the front */
+		detach(sel);
+		attach(sel);
 	}
 	focus(sel);
 	arrange(selmon);
